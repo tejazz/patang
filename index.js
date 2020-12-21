d396db492e1f47a09e400cc888ca07fc6d5a8045
tejaz <Tarique.Ejaz@agoda.com>
2020-12-21 12:25:35 +0700

    exposed constants used

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const pageEvaluator = require('./functions/pageEvaluator');
 const domEvaluator = require('./functions/domEvaluator');
-const platformConstants = require('./constants/productParams');
+const platformIdentifiers = require('./constants/productParams');
 
 module.exports = {
     pageEvaluator, 
     domEvaluator,
-    constants,
+    platformIdentifiers,
 };
